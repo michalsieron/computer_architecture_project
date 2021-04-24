1212eaabb1044da424e07bbafd325e962e6ee7c3
Michał Sieroń <michalwsieron@gmail.com>
2021-04-24 02:44:47 +0200

    Extended stored bits for rounding to 24
    Now all cases should be covered

--- src/fadd/fadd.v
+++ src/fadd/fadd.v
@@ -24,24 +24,24 @@ wire select = a[30:0] < b[30:0];
 wire [7:0] exponent_diff_abs = exponent_diff[7] ? -exponent_diff : exponent_diff;
 
 // fraction muxes
-wire [27:0] fraction_larger = {select ? fraction_b : fraction_a, 3'b000};
-wire [27:0] fraction_tmp = {select ? fraction_a : fraction_b, 3'b000};
+wire [48:0] fraction_larger = {select ? fraction_b : fraction_a, 24'b0};
+wire [48:0] fraction_tmp = {select ? fraction_a : fraction_b, 24'b0};
 
 // fraction shift right
-wire [27:0] fraction_smaller = fraction_tmp >> exponent_diff_abs;
+wire [48:0] fraction_smaller = fraction_tmp >> exponent_diff_abs;
 
 // sign muxes
 wire sign_larger = select ? sign_b : sign_a;
 
 // big alu
-wire [27:0] fraction_prenorm = sign_a ^ sign_b ? fraction_larger - fraction_smaller : fraction_larger + fraction_smaller;
+wire [48:0] fraction_prenorm = sign_a ^ sign_b ? fraction_larger - fraction_smaller : fraction_larger + fraction_smaller;
 
 // exponent mux
 wire [7:0] exponent_larger = select ? exponent_b : exponent_a;
 
 // normalise block
 integer index;
-reg [27:0] fraction_postnorm;
+reg [48:0] fraction_postnorm;
 reg [7:0] exponent_postnorm;
 
 always @(*) begin
@@ -50,13 +50,13 @@ always @(*) begin
     exponent_postnorm = exponent_larger;
     if (sign_a ^ sign_b) begin
         for (index = 22; index >= 0; index--) begin
-            if (fraction_postnorm[26] == 0) begin
+            if (fraction_postnorm[47] == 0) begin
                 fraction_postnorm = fraction_postnorm << 1;
                 exponent_postnorm = exponent_postnorm - 1;
             end
         end
     end else begin
-        if (fraction_prenorm[27]) begin
+        if (fraction_prenorm[48]) begin
             fraction_postnorm = fraction_prenorm >> 1;
             exponent_postnorm = exponent_larger + 1;
         end else begin
@@ -80,16 +80,17 @@ always @(*) begin
     //     out = {sign_larger, 8'hFF, 23'h0};
     // end else
 
-    if (fraction_postnorm[2] & |fraction_postnorm[1:0]) begin
-        fraction_postnorm[27:3] = fraction_postnorm[27:3] + 1;
-    end else if (&fraction_postnorm[3:2] & !(|fraction_postnorm[1:0])) begin
-        fraction_postnorm[27:3] = fraction_postnorm[27:3] + 1;
+// rounding
+    if (fraction_postnorm[23] & |fraction_postnorm[22:0]) begin
+        fraction_postnorm[48:24] = fraction_postnorm[48:24] + 1;
+    end else if (&fraction_postnorm[24:23] & !(|fraction_postnorm[22:0])) begin
+        fraction_postnorm[48:24] = fraction_postnorm[48:24] + 1;
     end
 
     if (exponent_postnorm == 8'd0) begin
         out = 32'h0;
     end else begin
-        out = {sign_larger, exponent_postnorm, fraction_postnorm[25:3]};
+        out = {sign_larger, exponent_postnorm, fraction_postnorm[46:24]};
     end
 end
 
