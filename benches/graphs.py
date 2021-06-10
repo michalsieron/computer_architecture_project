e1446d4bb9e191bed493940124123eb8fada0ef4
Michał Sieroń <michalwsieron@gmail.com>
2021-06-10 20:58:42 +0200

    Add graph generation

--- benches/graphs.py
+++ benches/graphs.py
@@ -6,61 +6,80 @@ data_a = np.genfromtxt("add_out.csv", delimiter=",")
 data_s = np.genfromtxt("sub_out.csv", delimiter=",")
 data_m = np.genfromtxt("mult_out.csv", delimiter=",")
 
-
-fig, ax = plt.subplots(2, 3)
-
 # relative difference
 
+fig1 = plt.figure(figsize=(12, 10), dpi=100)
+
 xa = data_a[:, 2]
-ax[0, 0].set_title("Add relative")
 ya1 = np.fromiter(((r[2] - r[0]) / r[2] for r in data_a), dtype=float)
-ax[0, 0].scatter(xa, ya1, s=1)
+plt.scatter(xa, ya1, s=1)
 
 ya2 = np.fromiter(((r[2] - r[1]) / r[2] for r in data_a), dtype=float)
-ax[0, 0].scatter(xa, ya2, s=1)
+plt.scatter(xa, ya2, s=1)
+
+plt.figlegend(["Zdenormalizowane", "IEEE-754"], loc="upper right", bbox_to_anchor=(0.85, 0.85))
+plt.savefig("add_relative.png", bbox_inches="tight")
+
+fig2 = plt.figure(figsize=(12, 10), dpi=100)
 
 xs = data_s[:, 2]
-ax[0, 1].set_title("Subtract relative")
 ys1 = np.fromiter(((r[2] - r[0]) / r[2] for r in data_s), dtype=float)
-ax[0, 1].scatter(np.abs(xs), ys1, s=1)
+plt.scatter(np.abs(xs), ys1, s=1)
 
 ys2 = np.fromiter(((r[2] - r[1]) / r[2] for r in data_s), dtype=float)
-ax[0, 1].scatter(np.abs(xs), ys2, s=1)
+plt.scatter(np.abs(xs), ys2, s=1)
+
+plt.figlegend(["Zdenormalizowane", "IEEE-754"], loc="upper right", bbox_to_anchor=(0.85, 0.85))
+plt.savefig("sub_relative.png", bbox_inches="tight")
+
+fig3 = plt.figure(figsize=(12, 10), dpi=100)
 
 xm = data_m[:, 2]
-ax[0, 2].set_title("Multiply relative")
 ym1 = np.fromiter(((r[2] - r[0]) / r[2] for r in data_m), dtype=float)
-ax[0, 2].scatter(xm, ym1, s=1)
+plt.scatter(xm, ym1, s=1)
 
 ym2 = np.fromiter(((r[2] - r[1]) / r[2] for r in data_m), dtype=float)
-ax[0, 2].scatter(xm, ym2, s=1)
+plt.scatter(xm, ym2, s=1)
+
+plt.figlegend(["Zdenormalizowane", "IEEE-754"], loc="upper right", bbox_to_anchor=(0.85, 0.85))
+plt.savefig("mul_relative.png", bbox_inches="tight")
 
 # absolute (ulp) difference
 
+fig4 = plt.figure(figsize=(12, 10), dpi=100)
+
 xa = data_a[:, 2]
-ax[1, 0].set_title("Add ULP")
 ya1 = np.fromiter((abs(r[2] - r[0]) / 2 ** 24 for r in data_a), dtype=float)
-ax[1, 0].scatter(xa, ya1, s=1)
+plt.scatter(xa, ya1, s=1)
 
 ya2 = np.fromiter((abs(r[2] - r[1]) / 2 ** 24 for r in data_a), dtype=float)
-ax[1, 0].scatter(xa, ya2, s=1)
+plt.scatter(xa, ya2, s=1)
+
+plt.figlegend(["Zdenormalizowane", "IEEE-754"], loc="upper left", bbox_to_anchor=(0.15, 0.85))
+plt.savefig("add_ulp.png", bbox_inches="tight")
+
+fig5 = plt.figure(figsize=(12, 10), dpi=100)
 
 xs = data_s[:, 2]
-ax[1, 1].set_title("Subtract ULP")
 ys1 = np.fromiter((abs(r[2] - r[0]) / 2 ** 24 for r in data_s), dtype=float)
-ax[1, 1].scatter(np.abs(xs), ys1, s=1)
+plt.scatter(np.abs(xs), ys1, s=1)
 
 ys2 = np.fromiter((abs(r[2] - r[1]) / 2 ** 24 for r in data_s), dtype=float)
-ax[1, 1].scatter(np.abs(xs), ys2, s=1)
+plt.scatter(np.abs(xs), ys2, s=1)
+
+plt.figlegend(["Zdenormalizowane", "IEEE-754"], loc="upper left", bbox_to_anchor=(0.15, 0.85))
+plt.savefig("sub_ulp.png", bbox_inches="tight")
+
+fig6 = plt.figure(figsize=(12, 10), dpi=100)
 
 xm = data_m[:, 2]
-ax[1, 2].set_title("Multiply ULP")
 ym1 = np.fromiter((abs(r[2] - r[0]) / 2 ** 24 for r in data_m), dtype=float)
-ax[1, 2].scatter(xm, ym1, s=1)
+plt.scatter(xm, ym1, s=1)
 
 ym2 = np.fromiter((abs(r[2] - r[1]) / 2 ** 24 for r in data_m), dtype=float)
-ax[1, 2].scatter(xm, ym2, s=1)
+plt.scatter(xm, ym2, s=1)
 
-fig.tight_layout()
-plt.figlegend(["Denormalized", "IEEE 754"], loc="lower center", ncol=2)
-plt.show()
+plt.figlegend(["Zdenormalizowane", "IEEE-754"], loc="upper left", bbox_to_anchor=(0.15, 0.85))
+plt.savefig("mul_ulp.png", bbox_inches="tight")
+
+# plt.show()
